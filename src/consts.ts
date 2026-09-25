957fc17e19509017dfec3b6107a3acd2e84b496a
Hamidi Nassim <nassimhamidi.unistudy@gmail.com>
2026-09-26 07:48:43 +0800

    Update contact information and social links

--- src/consts.ts
+++ src/consts.ts
@@ -32,10 +32,6 @@ export const LINKS: Links = [
     HREF: "/", 
   },
   { 
-    TEXT: "Work", 
-    HREF: "/work", 
-  },
-  { 
     TEXT: "Blog", 
     HREF: "/blog", 
   },
@@ -50,26 +46,26 @@ export const SOCIALS: Socials = [
   { 
     NAME: "Email",
     ICON: "email", 
-    TEXT: "markhorn.dev@gmail.com",
-    HREF: "mailto:markhorn.dev@gmail.com",
+    TEXT: "nassimhamidi.unistudy@gmail.com",
+    HREF: "mailto:nassimhamidi.unistudy@gmail.com",
   },
   { 
     NAME: "Github",
     ICON: "github",
-    TEXT: "markhorn-dev",
-    HREF: "https://github.com/markhorn-dev/astro-sphere"
+    TEXT: "nassim-hmdii",
+    HREF: "https://github.com/nassim-hmdii"
   },
   { 
     NAME: "LinkedIn",
     ICON: "linkedin",
-    TEXT: "markhorn-dev",
-    HREF: "https://www.linkedin.com/in/markhorn-dev/",
+    TEXT: "nassim-hamidi",
+    HREF: "https://www.linkedin.com/in/nassim-hamidi/",
   },
   { 
     NAME: "Twitter",
     ICON: "twitter-x",
-    TEXT: "markhorn_dev",
-    HREF: "https://twitter.com/markhorn_dev",
+    TEXT: "nassim-hamidi",
+    HREF: "https://twitter.com",
   },
 ]
 
